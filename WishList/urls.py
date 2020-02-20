a9f75a1979f4332e15a0b78f17ee2c36cc15f42a
moe9195 <moerahmeh95@hotmail.com>
2020-02-20 15:45:33 +0200

    final

--- WishList/urls.py
+++ WishList/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from wishlist import views
+from wish_list import views
 from django.conf import settings
 from django.conf.urls.static import static
 
